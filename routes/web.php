fa16706c4d03ad56e6b2450e9bc9783044727f12
Ian Hart <ianhart7790@gmail.com>
2020-11-22 19:54:18 -0500

    trying to clear route

--- routes/web.php
+++ routes/web.php
@@ -66,9 +66,9 @@ Route::patch('/users/following/{id}', [UserController::class, 'updateRemoveFromF
 
 // SNOWBOARDS
 Route::get('/snowboards', [SnowboardController::class, 'index'])->middleware('auth');
+Route::post('/snowboards', [SnowboardController::class, 'createSort']);
 Route::get('/snowboards/create', [SnowboardController::class, 'showSnowboardForm'])->middleware('auth');
 Route::post('/snowboards/create', [SnowboardController::class, 'createSnowboardForm']);
-Route::post('/snowboards', [SnowboardController::class, 'createSort']);
 Route::get('/snowboards/{id}', [SnowboardController::class, 'showDetails'])->middleware('auth');
 
 // REVIEWS
